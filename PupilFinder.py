1753b173b63e1863bb2110d419dd057c8209bd29
Thomas Haslwanter <thomas.haslwanter@fh-linz.at>
2013-07-18 11:12:52 +0200

    1.0 Version of PupilFinder, and required WMF-file

--- PupilFinder.py
+++ PupilFinder.py
@@ -1,6 +1,5 @@
-'''First version of finding a pupil.
+'''Simple program for finding a pupil.
 
-Not fast, not pretty, but gets the job done.
 - Finds a pupil center
 - indicates the center
 - generates an outplot
@@ -8,54 +7,71 @@ Not fast, not pretty, but gets the job done.
 
 Author: Thomas Haslwanter
 Date:   July 2013
-Ver:    0.1
+Ver:    1.0
 
 '''
 
-from FindPupil import *
 import cv2
 import os
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-def findCenter(frame):    
+def adaptThreshold(img, thresh):
+    ''' Adapt threshold '''
+    h,w = img.shape
+    lower = h*w/50
+    upper = h*w/4
+
+    bw = img < thresh
+
+    while np.sum(bw) < lower:
+        thresh += 5
+        bw  = img<thresh
+    while np.sum(bw) < upper:
+        thresh += 5
+        bw  = img<thresh
+
+    return (bw, thresh)
+
+
+def findCenter(frame, threshold):    
     '''Take a frame, and find the pupil center'''
     img = frame[...,0]
     h,w = img.shape
     
-    # Set an (adaptive) threshold. Note that the adaptation goes only into one direction!
-    threshold = 70
-    bw  = img<threshold
-    while np.sum(bw) < 10000:
-        threshold += 5
-        bw  = img<threshold
+    # Threshold the image and adapt - if necessary - the threshold
+    (bw, threshold) = adaptThreshold(img, threshold)
         
     # Flip b/w, and convert to uint8
     im_bw = np.array(~bw, dtype=np.uint8)*255
     
     # Fill the corners
+    # Note that the mask has to be 2 pixels larger than the image!
     mask = np.zeros( (h+2,w+2), dtype=np.uint8)
     cv2.floodFill(im_bw, mask, (1,479), 255)
     
+    # After the floodfill operation, the "mask" indicates which areas have
+    # been filled. It therefore has to be re-set for the next filling task.
     mask = np.zeros( (h+2,w+2), dtype=np.uint8)
     cv2.floodFill(im_bw, mask, (639,479), 255)
     
-    # Fill the holes
+    # Fill the holes in the pupil
     wHoles = im_bw.copy()
-    # Note that the mask has to be 2 pixels larger than the image!
     mask = np.zeros( (h+2,w+2), dtype=np.uint8)
     cv2.floodFill(wHoles, mask, (1,1), 0)
     im_bw[wHoles==255] = 0
     
     # Close the image, to remove the eye-brows    
-    strEl = makeCircle(25)
-    closed = close(im_bw, strEl)
+    radius = 25
+    strEl = np.zeros((2*radius+1, 2*radius+1), dtype=np.uint8)
+    cv2.circle(strEl, (radius,radius), radius, 255, -1)
+    
+    closed = cv2.morphologyEx(im_bw, cv2.MORPH_CLOSE, strEl)
     
     # find the edge
-    thrs1 = 1000
-    thrs2 = 1000
-    edge = cv2.Canny(closed, thrs1, thrs2, apertureSize=5)
+    edgeThresholds = (1000, 1000)
+    edge = cv2.Canny(closed, edgeThresholds[0], edgeThresholds[1], apertureSize=5)
     
     # find the center of the edge
     edge_y, edge_x = np.where(edge==255)
@@ -63,9 +79,11 @@ def findCenter(frame):
     if np.any(np.isnan(center)):
         center = np.zeros(2)
         
-    return center
+    return (center, threshold)
         
 if __name__ == '__main__':
+    ''' Main function '''
+
     # Get the data
     inDir = '.'
     inFile = 'VIDEO1 LEFT POSTERIOR CANAL BPV.wmv'
@@ -81,17 +99,18 @@ if __name__ == '__main__':
     cv2.namedWindow('video')
     
     # Just for development, so I don't have to wait so long:
-    maxLength = 100
+    numFrames = min(10000, numFrames)
     
     # Allocate the memory
-    centerPupil = np.zeros((maxLength,2))
+    centerPupil = np.nan*np.ones((numFrames,2))
     ii = 0
+    threshold = 70
     
-    while success and cv2.waitKey(1) and ii < maxLength:
-        sys.stdout.write("\rPercent done: %d  " % (ii*100/maxLength))
+    while success and cv2.waitKey(1) and ii < numFrames:
+        sys.stdout.write("\rPercent done: %d  " % (ii*100/numFrames))
         
         # Find the pupil center
-        centerPupil[ii] = findCenter(frame)
+        centerPupil[ii], threshold = findCenter(frame, threshold)
         
         # Show the current frame, with the center indicated
         cv2.circle(frame, (int(centerPupil[ii,0]), int(centerPupil[ii,1])), 10, (0, 0,255))
@@ -104,16 +123,18 @@ if __name__ == '__main__':
     # Close the CV-window    
     cv2.destroyAllWindows()
     
+    # Eliminate un-detected pupil locations
+    centerPupil[centerPupil==0] = np.nan
+
+    # Save the data to an outfile
+    outFile = 'pupilcenter.txt'
+    np.savetxt(outFile, centerPupil)
+    print('\nOutput written to {0}'.format(outFile))
+
     # Make a plot of the center position
-    time = np.arange(maxLength)/float(fps)
+    time = np.arange(numFrames)/float(fps)
     plt.plot(time, centerPupil[:,0], label='horizontal')
     plt.hold(True)
     plt.plot(time, centerPupil[:,1], label='vertical')
     plt.legend()
     plt.show()
-    
-    # Save the data to an outfile
-    outFile = 'pupilcenter.txt'
-    np.savetxt(outFile, centerPupil)
-    print('Output written to {0}'.format(outFile))
-    raw_input('Done')
